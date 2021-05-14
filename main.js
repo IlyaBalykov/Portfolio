e73abd712a3c2393e601e72b6143dd6cf1fb4544
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-05-14 18:09:53 +0300

    Adaptability and cross-browser compatibility

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let window_flashcards = document.querySelector('.window-flashcards');
 let window_virtual_piano = document.querySelector('.window-virtual-piano');
+let hamburger_menu = document.getElementById('hamburger');
+
 function flashcards(oldToken, newToken) {
     window_flashcards.classList.replace('window-flashcards', 'open-window-flashcards');
 }
@@ -11,4 +13,13 @@ function virtual_piano(oldToken, newToken) {
 }
 function virtual_piano_close(oldToken, newToken) {
     window_virtual_piano.classList.replace('open-window-virtual-piano', 'window-virtual-piano');
+}
+function hamburger() {
+    hamburger_menu.classList.replace('hamburger', 'open-hamburger-menu');
+}
+function hamburger_close() {
+    hamburger_menu.classList.replace('open-hamburger-menu','hamburger');
+}
+icon.onclick = function () {
+    this.classList.toggle('icon-click');
 }
